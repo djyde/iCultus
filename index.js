657047b00a6ac6c6f8645bfb0dfda70b458f18cc
Randy <randypriv@gmail.com>
2015-11-05 23:09:53 +0800

    unresizable window

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const menubar = require('menubar')
 const mb = new menubar({
   openDevTools: true,
   'always-on-top': true,
-  devMode: true
+  devMode: true,
+  resizable: false,
+  width: 250,
+  height: 300
 })
 
 mb.on('ready', () => console.log('ready'))
